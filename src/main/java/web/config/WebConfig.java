e9009c3c17e730ec64305dba052af2bbffce971b
keepclimbs <499429059@qq.com>
2018-09-21 17:11:11 +0800

    删除springMVC配置

--- src/main/java/web/config/WebConfig.java
+++ src/main/java/web/config/WebConfig.java
@@ -13,16 +13,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 // 配置类注解
 @Configuration
-// 启用springMVC (具体作用目前就知道 让configureDefaultServletHandling 方法生效)
-@EnableWebMvc
 // basePackages会扫描当前包下的所有文件 可以多个包 { "web", "web1"} 必须要有大括号 不然无法加载
 @ComponentScan(basePackages = {"web"})
 // 实现 WebMvcConfigurer 是为了配置 静态资源的处理
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    // 静态资源的处理 如果没有这个 页面就引用不到css js等静态文件
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
 }
